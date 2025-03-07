85157663123f3fcf1a29b3e5fbd6921646ebb40b
Fern-Aerell <fernaerell2020@gmail.com>
2025-03-07 17:43:18 +0700

    learn: Basic structure c program

---
+++ basic/basic_structure_c_program/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Import input and output standard library in c
+
+// Main function
+int main() {
+  printf("Hello, World!\n"); // Print text to screen
+  return 0; // Return 0 (program success no error.)
+}
